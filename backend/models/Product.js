812382eaf22b25d98672ab962eec26bacdd01a36
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-02-24 12:32:20 -0300

    feat: added product variation array

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose")
 
+const VariationSchema = new mongoose.Schema({
+    cor: { type: String, required: false },
+    tamanho: { type: String, required: false },
+    imagem: { type: String, required: true },
+})
 
 const ProductSchema = new mongoose.Schema(
     {
         titulo:{type: String, required:true},
         descricao:{type:String, required:true},
-        imagem:{type:String, required: true},
+        variacoes: {type: [VariationSchema], required: true },
         categorias:{type:Array},
-        tamanho:{type:Array},
-        cor:{type:Array},
         preco:{type:Number, required: true},
         inStock:{type: Boolean, default: true}
     },
